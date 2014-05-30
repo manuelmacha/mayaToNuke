13f715822560c7b6d0586298e150aa4cc4fbec4d
Manuel Macha <manuel@manuelmacha.de>
2014-05-30 22:17:52 +1000

    annotations

--- src/mayaToNuke/utils/appContext.py
+++ src/mayaToNuke/utils/appContext.py
@@ -1,27 +1,27 @@
 import sys
 
 class AppContext(object):
-    _instance = None
-    def __init__(self):
+    _instance = None # 1.)
+    def __init__(self): # 3.)
         self.__insideMaya  = 'maya.app.startup.basic' in sys.modules
         self.__insideNuke = 'nuke' in sys.modules
         self.__standalone = not (self.__insideMaya or self.__insideNuke)
         
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs): # 2.)
         if not cls._instance:
             cls._instance = super(AppContext, cls).__new__(cls, *args, **kwargs)
         return cls._instance        
         
-    def isMaya(self):
+    def isMaya(self): # 4.)
         return self.__insideMaya
     
-    def isNuke(self):
+    def isNuke(self): # 4.)
         return self.__insideNuke
     
-    def isStandalone(self):
+    def isStandalone(self): # 4.)
         return self.__standalone
     
-    def toString(self):
+    def toString(self): # 5.)
         if self.isMaya(): return 'Maya'
         elif self.isNuke(): return 'Nuke'
         return 'Standalone'
